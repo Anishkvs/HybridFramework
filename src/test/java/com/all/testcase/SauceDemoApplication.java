3fb797a46568e4489af3b9b04f6285bf929cfe8e
Anishkvs <anishkumarvijayan@gmail.com>
2023-09-15 16:29:15 +0530

    v1.5

---
+++ src/test/java/com/all/testcase/SauceDemoApplication.java
@@ -0,0 +1,15 @@
+package com.all.testcase;
+
+
+
+public class SauceDemoApplication {
+	
+	public static void main(String[] args) throws InterruptedException {
+
+	//In progress...
+		
+	}
+
+	
+
+}
